6cdf190c7a27384c86c4e9fc3d64b567f1ee8e2d
Dirk Eddelbuettel <edd@debian.org>
2024-12-26 09:58:23 -0600

    Update examples

--- inst/examples/adbcExFlights.R
+++ inst/examples/adbcExFlights.R
@@ -1,21 +1,28 @@
 
 ## Use duckdb with the adbc driver
-library(adbcdrivermanager)
-library(duckdb)
+suppressMessages({
+    library(adbcdrivermanager)
+    library(duckdb)
+    library(naarma)
+    #library(bit64)  # to test with as.integer64
+})
+
 db <- adbc_database_init(duckdb_adbc())
 
 con <- adbc_connection_init(db)
 
 # Write a table
-write_adbc(nycflights13::flights, con, "flights")
-#write_adbc(data.frame(ind=seq(1.0,22.0,by=1.0), val=seq(22,1,by=-1)), con, "flights")
+#write_adbc(nycflights13::flights, con, "flights")
+#write_adbc(data.frame(ind = as.integer64(seq(1.0,2200.0,by=1.0)),
+#                      val = as.integer64(seq(2200,1,by=-1))),
+#           con, "flights")
+write_adbc(data.frame(ind = 1:2200, val=seq(2200,1,by=-1)), con, "flights")
 
 ## Query duckdb via adbc, get arrow streams, convert those
 #nadata <- read_adbc(con, "SELECT * from flights")  # return a array stream object
 #D <- nanoarrow::convert_array_stream(nadata)  	# now D is a data.frame
 #print(str(D))
 
-library(naarma)
 nadata <- read_adbc(con, "SELECT * FROM flights")  # return a array stream object
 
 #print(nadata)
--- inst/examples/adbcExMlpack.R
+++ inst/examples/adbcExMlpack.R
@@ -1,7 +1,9 @@
 
-## Use duckdb with the adbc driver
-library(adbcdrivermanager)
-library(duckdb)
+## Use duckdb with the adbc driver (suppressing the automatic 'Loading ... DBI')
+suppressMessages({
+    library(adbcdrivermanager)
+    library(duckdb)
+})
 db <- adbc_database_init(duckdb_adbc())
 con <- adbc_connection_init(db)
 
@@ -12,16 +14,20 @@ execute_adbc(con, "CREATE TABLE covertypeLabels AS SELECT * FROM 'https://www.ml
 
 ## Query duckdb via adbc, get arrow streams, convert those
 nadata <- read_adbc(con, "SELECT * from covertypeData")  # return a array stream object
-D <- nanoarrow::convert_array_stream(nadata)  	# now D is a data.frame
+#D <- nanoarrow::convert_array_stream(nadata)  	# now D is a data.frame
+D <- naarma::collectFromStream(nadata)
+
 nalabels <- read_adbc(con, "SELECT * from covertypeLabels")  # return a array stream object
-L <- nanoarrow::convert_array_stream(nalabels)  # now L is a one-column data.frame
+#L <- nanoarrow::convert_array_stream(nalabels)  # now L is a one-column data.frame
+L <- naarma::collectFromStream(nalabels)
+
 
 trainInd <- 1:70000
 testInd <- 70001:100000
-fit <- mlpack::random_forest(training=as.matrix(D[trainInd,]),
-                             labels=as.matrix(L[trainInd, "column0"]),
-                             num_trees=10, minimum_leaf_size=3,
-                             print_training_accuracy=TRUE,
-                             test=as.matrix(D[testInd,]),
-                             test_labels=as.matrix(L[testInd, "column0"]),
+fit <- mlpack::random_forest(training = D[trainInd,],
+                             labels = L[trainInd, 1, drop=FALSE],
+                             num_trees = 10, minimum_leaf_size = 3,
+                             print_training_accuracy = TRUE,
+                             test = D[testInd,],
+                             test_labels = L[testInd, 1, drop=FALSE],
                              verbose=TRUE)
