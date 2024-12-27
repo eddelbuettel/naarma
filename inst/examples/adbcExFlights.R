0e10073d0d5d5d71386052a27e0b3888636a749c
Dirk Eddelbuettel <edd@debian.org>
2024-12-27 16:54:13 -0600

    Back to flights

--- inst/examples/adbcExFlights.R
+++ inst/examples/adbcExFlights.R
@@ -12,11 +12,11 @@ db <- adbc_database_init(duckdb_adbc())
 con <- adbc_connection_init(db)
 
 # Write a table
-#write_adbc(nycflights13::flights, con, "flights")
+write_adbc(nycflights13::flights, con, "flights")
 #write_adbc(data.frame(ind = as.integer64(seq(1.0,2200.0,by=1.0)),
 #                      val = as.integer64(seq(2200,1,by=-1))),
 #           con, "flights")
-write_adbc(data.frame(ind = 1:2200, val=seq(2200,1,by=-1)), con, "flights")
+#write_adbc(data.frame(ind = 1:2200, val=seq(2200,1,by=-1)), con, "flights")
 
 ## Query duckdb via adbc, get arrow streams, convert those
 #nadata <- read_adbc(con, "SELECT * from flights")  # return a array stream object
